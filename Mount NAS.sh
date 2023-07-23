76fc91930c5479e3a2d39a6ccb0e47aa012b14a8
Roman Kis <roman.kiss.scuola@gmail.com>
2023-07-23 10:00:31 +0200

    Update Mount NAS.sh
    fixed support for cifs 2.0

--- Mount NAS.sh
+++ Mount NAS.sh
@@ -10,11 +10,11 @@ fi
 sudo apt-get install cifs-utils
 
 echo ""
-read -r -p "Enter the address of the NAS folder that you want to mount: " nasaddress
+read -r -p "Enter the address of the NAS folder that you want to mount (//192.168.1.10/homes/users): " nasaddress
 echo ""
-read -r -p "Enter the local path where you want to mount the NAS folder: " localaddress
+read -r -p "Enter the local path where you want to mount the NAS folder(/mnt/nas): " localaddress
 echo ""
-read -r -p "Enter the path to store credentials file: " credentialaddress
+read -r -p "Enter the path to store credentials file (/etc/nascreds): " credentialaddress
 echo ""
 if [ ! -d $localaddress ]; then
     sudo mkdir -p $localaddress
@@ -34,7 +34,7 @@ password=$naspassword
 _EOF_
 
 echo ""
-echo "$nasaddress  $localaddress  cifs  credentials=$credentialaddress/.NASCREDS,uid=1000,gid=1000,vers=1.0  0  01" >> /etc/fstab
+echo "$nasaddress  $localaddress  cifs  credentials=$credentialaddress/.NASCREDS,uid=1000,gid=1000,vers=2.0  0  01" >> /etc/fstab
 echo ""
 echo ""
 echo "@reboot root /bin/bash -c 'sleep 10 && /bin/mount -a'" >> /etc/crontab
